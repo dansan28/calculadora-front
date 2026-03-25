ab197862a714e268ea3d6b0882dc01983b5756bf
dansan28 <danielsancas04@gmail.com>
2026-03-24 20:47:46 -0600

    correccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import { GlobalProvider } from './context/GlobalState'
 import Balance from './components/Balance'
 import IngresosEgresos from './components/IngresosEgresos'
